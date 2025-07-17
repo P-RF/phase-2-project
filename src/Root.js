2c30e66a13da7f2847cf589baea5fecfebc3a04d
Priscila Rodriguez <priscila.rodriguezfresco@gmail.com>
2025-07-17 18:41:11 -0400

    updated Main.css and Roots.js

--- src/Root.js
+++ src/Root.js
@@ -13,7 +13,21 @@ export default function Root() { // useTasks handles Fetch CRUD - Fetch requests
   return (    // pass props to child components via useContext(TaskContent) -- Global state & client-side routing
     <TasksContext.Provider value={{tasks, addTask, toggleTask, deleteTask}}>
       <BrowserRouter>
-        <App />
+        <div
+          style={{
+            margin: 0,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+            backgroundImage: "url('/images/colored-squares.jpg')",
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+          }}
+        >
+          <App />
+        </div>
       </BrowserRouter>   
     </TasksContext.Provider>
   );
